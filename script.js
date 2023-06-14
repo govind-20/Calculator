f8fccf21ecc0545523b6837f56b042577a8ac7b0
Govind Sharma <84737099+govind-20@users.noreply.github.com>
2023-06-14 18:55:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+/*const op=prompt('enter teh operator');
+let a=parseFloat(prompt('enter first number'));
+let b=parseFloat(prompt('enter first number'));
+if (op=='+'){
+  result=a+b
+}
+if (op=='+'){
+  result=a+b
+}
+if (op=='-'){
+  result=a-b
+}
+if (op=='*'){
+  result=a*b
+}
+if (op=='/'){
+  result=a/b
+}
+console.log('result is',result)
+*/
